bd444e84f6a25842f0028c9ef6883b525b80ee3c
Kunal Goyal <kunalgoyal.goyal9@gmail.com>
2018-05-10 11:56:14 +0530

    Add files via upload
    Create 5 problem template ready in specified path (i.e. main.cc in my case)

---
+++ bgn.sh
@@ -0,0 +1,9 @@
+echo "contest name"
+read contest
+mkdir $contest
+cd $contest
+for i in 1 2 3 4 5
+do
+	touch $i".cpp"
+    cat /home/kunal_goyal/Desktop/codeforces-master/main.cc > $i".cpp"
+done 
